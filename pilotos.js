1ddfa491d021d8de7c79db3c8720964a1cf846a9
Martim Pereira <martimmarcelino1661@gmail.com>
2026-06-05 18:37:12 +0100

    pilotos feitos

--- pilotos.js
+++ pilotos.js
@@ -11,7 +11,7 @@ const pilotos = [
     id: 2,
     nome: "Isack Hadjar",
     equipa: "Red Bull",
-    numero: 5,
+    numero: 6,
     pais: "França",
     foto: "img/isack.jpg"
 },
@@ -38,6 +38,141 @@ const pilotos = [
     numero: 16,
     pais: "Mónaco",
     foto: "img/charles.jpg"
+},
+{
+    id: 6,
+    nome: "Lewis Hamilton",
+    equipa: "Ferrari",
+    numero: 44,
+    pais: "Reino Unido",
+    foto: "img/lewis.jpg"
+},
+{
+    id: 7,
+    nome: "George Russell",
+    equipa: "Mercedes",
+    numero: 63,
+    pais: "Reino Unido",
+    foto: "img/george.jpg"
+},
+{
+    id: 8,
+    nome: "Kimi Antonelli",
+    equipa: "Mercedes",
+    numero: 12,
+    pais: "Itália",
+    foto: "img/kimi.jpg"
+},
+{
+    id: 9,
+    nome: "Franco Colapinto",
+    equipa: "Alpine",
+    numero: 43,
+    pais: "Argentina",
+    foto: "img/franco.jpg"
+},
+{
+    id: 10,
+    nome: "Pierre Gasly",
+    equipa: "Alpine",
+    numero: 10,
+    pais: "França",
+    foto: "img/pierre.jpg"
+},
+{
+    id: 11,
+    nome: "Liam Lawson",
+    equipa: "Racing Bulls",
+    numero: 30,
+    pais: "Nova Zelândia",
+    foto: "img/liam.jpg"
+},
+{
+    id: 12,
+    nome: "Arvid Lindblad",
+    equipa: "Racing Bulls",
+    numero: 41,
+    pais: "Reino Unido",
+    foto: "img/arvid.jpg"
+},
+{
+    id: 13,
+    nome: "Esteban Ocon",
+    equipa: "Haas",
+    numero: 31,
+    pais: "França",
+    foto: "img/esteban.jpg"
+},
+{
+    id: 14,
+    nome: "Oliver Bearman",
+    equipa: "Haas",
+    numero: 87,
+    pais: "Reino Unido",
+    foto: "img/oliver.jpg"
+},
+{
+    id: 15,
+    nome: "Carlos Sainz",
+    equipa: "WIlliams",
+    numero: 55,
+    pais: "Espanha",
+    foto: "img/carlos.jpg"
+},
+{
+    id: 16,
+    nome: "Alexander Albon",
+    equipa: "Williams",
+    numero: 23,
+    pais: "Estados Unidos",
+    foto: "img/alexander.jpg"
+},
+{
+    id: 17,
+    nome: "Nico Hülkenberg",
+    equipa: "Audi",
+    numero: 27,
+    pais: "Alemanha",
+    foto: "img/nico.jpg"
+},
+{
+    id: 18,
+    nome: "Gabriel Bortoleto",
+    equipa: "Audi",
+    numero: 5,
+    pais: "Brasil",
+    foto: "img/gabriel.jpg"
+},
+{
+    id: 19,
+    nome: "Sergio Pérez",
+    equipa: "Cadillac",
+    numero: 11,
+    pais: "México",
+    foto: "img/sergio.jpg"
+},
+{
+    id: 20,
+    nome: "Valterri Bottas",
+    equipa: "Cadillac",
+    numero: 77,
+    pais: "Brasil",
+    foto: "img/valterri.jpg"
+},
+{
+    id: 21,
+    nome: "Fernando Alonso",
+    equipa: "Aston Martin",
+    numero: 14,
+    pais: "Espanha",
+    foto: "img/fernando.jpg"
+},
+{
+    id: 22,
+    nome: "Lance Stroll",
+    equipa: "Aston Martin",
+    numero: 18,
+    pais: "Canadá",
+    foto: "img/lance.jpg"
 }
-
 ];
