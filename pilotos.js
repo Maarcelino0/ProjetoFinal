75ce3383ec820157e068c353bf249707b142f24b
Martim Pereira <martimmarcelino1661@gmail.com>
2026-06-05 18:11:27 +0100

    first commit

---
+++ pilotos.js
@@ -0,0 +1,27 @@
+const pilotos = [
+{
+    id: 1,
+    nome: "Max Verstappen",
+    equipa: "Red Bull",
+    numero: 3,
+    pais: "Países Baixos",
+    foto: "img/max.jpg"
+},
+{
+    id: 2,
+    nome: "Isack Hadjar",
+    equipa: "Red Bull",
+    numero: 5,
+    pais: "França",
+    foto: "img/isack.jpg"
+},
+{
+    id: 3,
+    nome: "Lando Norris",
+    equipa: "McLaren",
+    numero: 1,
+    pais: "Reino Unido",
+    foto: "img/lando.jpg"
+},
+
+];
